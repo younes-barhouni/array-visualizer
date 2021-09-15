480ba71b2be80c0e94baf5679060c97d6bdcd809
BARHOUNI <younes-barhouni@users.noreply.github.com>
2021-09-15 21:30:57 +0200

    Update types

--- src/App/ArrayVis.ts
+++ src/App/ArrayVis.ts
@@ -1,192 +1,213 @@
-import * as d3 from "d3";
-import Highlight from "./Utils/HighLight";
+import * as d3 from 'd3';
+import Highlight from './Utils/HighLight';
 
-
+/**
+ * Array methods visualizer main class
+ */
 export default class ArrayVis {
+  private data: any[];
+  private svg;
+  private speed = 500;
+  private dataWidths: Iterable<d3.Numeric>;
+  private commaWidth: number;
+  private parensWidth: number;
+  private height: any;
 
-    private data: any[];
-    private svg;
-    private speed = 500;
-    private dataWidths;
-    private commaWidth;
-    private parensWidth;
-    private height;
+  public options = {};
+  public container;
 
-    public options = {};
-    public container;
+  constructor(
+    data: any[],
+    svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
+    options: { speed?: any; fontsize?: number }
+  ) {
+    this.data = data;
+    this.svg = svg;
+    this.speed = options.speed;
+    this.options = options;
+    this.container = this.svg
+      .append('g')
+      .attr('transform', (d: any, i: any) => 'translate(0,50)');
+    this.update(data);
+  }
 
-    constructor(data, svg, options) {
-        this.data = data;
-        this.svg = svg;
-        this.speed = options.speed;
-        this.options = options;
-        this.container = this.svg.append('g').attr("transform", (d, i) => "translate(0,50)");
-        this.update(data);
-    }
+  /**
+   *
+   * @param data
+   */
+  private computeWidths(data: any[]) {
+    const text = this.container
+      .selectAll('text._text')
+      .data(data)
+      .enter()
+      .append('text')
+      .text((d: any) => d);
 
-    /**
-     * 
-     * @param data 
-     */
-    private computeWidths(data) {
-        const text = this.container.selectAll('text._text')
-            .data(data)
-            .enter()
-            .append('text')
-            .text(d => d)
+    const commas = this.container.append('text').text(',');
 
-        const commas = this.container.append('text').text(',')
+    const parens = this.container.append('text').text('[');
 
-        const parens = this.container.append('text').text("[")
+    this.dataWidths = data.map((a: any, i: string | number) => {
+      return text['_groups'][0][i].getComputedTextLength();
+    });
 
-        this.dataWidths = data.map((a, i) => {
-            return text["_groups"][0][i].getComputedTextLength();
-        })
+    this.commaWidth = commas['_groups'][0][0].getComputedTextLength();
+    this.parensWidth = parens['_groups'][0][0].getComputedTextLength();
 
-        this.commaWidth = commas["_groups"][0][0].getComputedTextLength();
-        this.parensWidth = parens["_groups"][0][0].getComputedTextLength();
+    this.height = parens['_groups'][0][0].getBBox().height;
 
-        this.height = parens["_groups"][0][0].getBBox().height;
+    // debugger;
 
-        // debugger;
+    text.remove();
+    commas.remove();
+    parens.remove();
+  }
 
-        text.remove();
-        commas.remove();
-        parens.remove();
+  /**
+   *
+   * @param data
+   */
+  public update(data: any[]) {
+    this.computeWidths(data);
 
-    }
+    // DATA JOIN
+    const text = this.container.selectAll('text.text').data(data, (a: any) => {
+      return a;
+    });
 
-    /**
-     * 
-     * @param data 
-     */
-    public update(data) {
-        this.computeWidths(data);
+    const commas = this.container
+      .selectAll('text.comma')
+      .data(data.slice(0, data.length - 1));
 
-        // DATA JOIN
-        const text = this.container.selectAll('text.text')
-            .data(data, (a) => { return a });
+    const parens = this.container.selectAll('text.parens').data(['[', ']']);
 
-        const commas = this.container.selectAll('text.comma')
-            .data(data.slice(0, data.length - 1));
+    // UPDATE
+    text
+      .transition()
+      .duration(this.speed)
+      .text((d: any) => d)
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths.slice(0, i)) + this.parensWidth;
+        x += i * this.commaWidth;
+        return 'translate(' + x + ',0)';
+      });
 
-        const parens = this.container.selectAll('text.parens')
-            .data(['[', ']'])
+    commas
+      .transition()
+      .duration(this.speed)
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths.slice(0, i + 1)) + this.parensWidth;
+        x += i * this.commaWidth;
+        return 'translate(' + x + ',0)';
+      });
 
-        // UPDATE
-        text
-            .transition()
-            .duration(this.speed)
-            .text(d => d)
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths.slice(0, i)) + this.parensWidth;
-                x+= i * this.commaWidth;
-                return "translate(" + x + ",0)"
-            })
+    parens
+      .transition()
+      .duration(this.speed)
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths) + this.parensWidth;
+        x += (data.length - 1) * this.commaWidth;
+        if (data.length === 0) x = this.parensWidth;
+        return 'translate(' + (i === 0 ? 0 : x) + ',0)';
+      });
 
-        commas
-            .transition()
-            .duration(this.speed)
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths.slice(0, i + 1)) + this.parensWidth;
-                x+= i * this.commaWidth;
-                return "translate(" + x + ",0)"
-            })
+    // ENTER
+    text
+      .enter()
+      .append('text')
+      .text((d: any, i: any) => {
+        return d;
+      })
+      .attr('class', 'text')
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths.slice(0, i)) + this.parensWidth;
+        x += i * this.commaWidth;
+        return 'translate(' + x + ',0)';
+      })
+      .attr('opacity', 0)
+      .attr('y', 50)
+      .transition()
+      .duration(this.speed)
+      .attr('opacity', 1)
+      .attr('y', 0);
 
+    commas
+      .enter()
+      .append('text')
+      .text((d: any, i: any) => {
+        return ',';
+      })
+      .attr('class', 'comma')
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths.slice(0, i + 1)) + this.parensWidth;
+        x += i * this.commaWidth;
+        return 'translate(' + x + ',0)';
+      });
 
-        parens
-            .transition()
-            .duration(this.speed)
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths) + this.parensWidth;
-                x+= (data.length - 1) * this.commaWidth;
-                if (data.length === 0)
-                    x = this.parensWidth;
-                return "translate(" + (i === 0 ? 0 : x) + ",0)";
-            })
+    parens
+      .enter()
+      .append('text')
+      .attr('class', 'parens')
+      .attr('transform', (d: any, i: number) => {
+        let x = d3.sum(this.dataWidths) + this.parensWidth;
+        x += (data.length - 1) * this.commaWidth;
+        if (data.length === 0) x = this.parensWidth;
+        return 'translate(' + (i === 0 ? 0 : x) + ',0)';
+      })
+      .text((d: any) => d);
 
+    //EXIT
+    text
+      .exit()
+      .attr('y', 0)
+      .transition()
+      .duration(this.speed / 2)
+      .attr('y', -75)
+      .transition()
+      .duration(this.speed / 2)
+      .attr('opacity', 0)
+      .remove();
 
-        // ENTER
-        text.enter().append('text')
-            .text((d, i) => { return d })
-            .attr('class', 'text')
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths.slice(0, i)) + this.parensWidth;
-                x+= i * this.commaWidth;
-                return "translate(" + x + ",0)"
-            })
-            .attr('opacity', 0)
-            .attr('y', 50)
-            .transition()
-            .duration(this.speed)
-            .attr('opacity', 1)
-            .attr('y', 0)
+    commas.exit().remove();
+  }
 
-        commas.enter().append('text')
-            .text((d, i) => { return ","; })
-            .attr('class', 'comma')
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths.slice(0, i + 1)) + this.parensWidth;
-                x+= i * this.commaWidth;
-                return "translate(" + x + ",0)"
-            })
+  /**
+   *
+   * @param a
+   * @param index
+   */
+  public push(a: string | number, index?: number) {
+    this.data.splice(index || this.data.length, 0, a);
+    this.update(this.data);
+  }
 
-        parens
-            .enter().append('text')
-            .attr('class', 'parens')
-            .attr("transform", (d, i) => {
-                let x = d3.sum(this.dataWidths) + this.parensWidth;
-                x+= (data.length - 1) * this.commaWidth;
-                if (data.length === 0) x = this.parensWidth;
-                return "translate(" + (i === 0 ? 0 : x) + ",0)"
-            })
-            .text(d => d)
+  /**
+   *
+   * @param index
+   * @param value
+   */
+  public splice(index: number, value: any) {
+    value ? this.data.splice(index, 1, value) : this.data.splice(index, 1);
+    this.update(this.data);
+  }
 
-        //EXIT
-        text.exit()
-            .attr("y", 0)
-            .transition()
-            .duration(this.speed / 2)
-            .attr("y", -75)
-            .transition()
-            .duration(this.speed / 2)
-            .attr("opacity", 0)
-            .remove();
+  /**
+   *
+   * @param index
+   * @returns
+   */
+  public highlight(index: number) {
+    index = index || 0;
 
-        commas.exit()
-            .remove();
-    }
-
-    /**
-     * 
-     * @param a 
-     * @param index 
-     */
-    public push(a, index?){
-        this.data.splice(index || this.data.length, 0, a);
-        this.update(this.data);
-    }
-
-    /**
-     * 
-     * @param index 
-     * @param value 
-     */
-    public splice(index, value) {
-        value ? this.data.splice(index, 1, value) : this.data.splice(index, 1);
-        this.update(this.data);
-    }
-
-    /**
-     * 
-     * @param index 
-     * @returns 
-     */
-    public highlight(index) {
-        index = index || 0;
-
-        const highlight = new Highlight(index, this.height, this.container, this.data, this.dataWidths, this.parensWidth, this.commaWidth);
-        return highlight;
-    }
-
+    const highlight = new Highlight(
+      index,
+      this.height,
+      this.container,
+      this.data,
+      this.dataWidths,
+      this.parensWidth,
+      this.commaWidth
+    );
+    return highlight;
+  }
 }
